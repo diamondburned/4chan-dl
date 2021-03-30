f8391cc6080662b8e7022abe1297350e172a2bc5
diamondburned <datutbrus@gmail.com>
2021-03-29 23:58:17 -0700

    derive fork, add sema

--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
-module chandl
+module github.com/diamondburned/chandl
 
 go 1.16
 
 require (
 	github.com/PuerkitoBio/goquery v1.6.1
 	golang.org/x/net v0.0.0-20210316092652-d523dce5a7f4 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 )
--- main.go
+++ main.go
@@ -1,56 +1,88 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"context"
+	"flag"
+	"io"
+	"log"
 	"net/http"
 	"os"
-	"strings"
+	"path"
+	"runtime"
 	"sync"
 
 	"github.com/PuerkitoBio/goquery"
+	"golang.org/x/sync/semaphore"
 )
 
-var c http.Client
+var (
+	tasks = runtime.GOMAXPROCS(-1) * 16
+)
+
+func init() {
+	flag.IntVar(&tasks, "tasks", tasks, "number of concurrent tasks to perform")
+	flag.Parse()
+}
 
 func main() {
-	var wg sync.WaitGroup
-	res, err := http.Get(os.Args[1])
+	r, err := http.Get(os.Args[1])
 	if err != nil {
-		panic(err)
+		log.Fatalln("failed to get URL:", err)
+		return
 	}
-	defer res.Body.Close()
-	doc, err := goquery.NewDocumentFromReader(res.Body)
+
+	defer r.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(r.Body)
 	if err != nil {
-		panic(err)
+		log.Fatalln("failed to parse HTML:", err)
 	}
+	r.Body.Close()
+
+	wg := sync.WaitGroup{}
+	sema := semaphore.NewWeighted(int64(tasks))
+
 	doc.Find("a.fileThumb").Each(func(i int, s *goquery.Selection) {
+		href, ok := s.Attr("href")
+		if !ok {
+			return
+		}
+
+		sema.Acquire(context.Background(), 1)
 		wg.Add(1)
+
 		go func() {
-			a, _ := s.Attr("href")
-			fmt.Println(a)
-			downloadandsave(a)
+			downloadAndSave(href)
+			sema.Release(1)
 			wg.Done()
 		}()
 	})
-	
+
 	wg.Wait()
 }
 
-func downloadandsave(s string) {
-	str := "https:" + s
-	fname := strings.Split(str, "/")[len(strings.Split(str, "/"))-1]
-	if _, err := os.Stat(fname); !os.IsNotExist(err) {
+func downloadAndSave(s string) {
+	s = "https:" + s
+
+	if _, err := os.Stat(path.Base(s)); err == nil {
 		return
 	}
-	res, err := c.Get(str)
+
+	r, err := http.Get(s)
 	if err != nil {
-		panic(err)
+		log.Printf("failed to fetch %s: %v\n", s, err)
+		return
 	}
-	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
-	err = ioutil.WriteFile("./"+fname, body, 0666)
+	defer r.Body.Close()
+
+	f, err := os.Create(path.Base(s))
 	if err != nil {
-		panic(err)
+		log.Println("failed to create file:", err)
+		return
+	}
+	defer f.Close()
+
+	if _, err = io.Copy(f, r.Body); err != nil {
+		log.Println("failed to download:", err)
 	}
 }
